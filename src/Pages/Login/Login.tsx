bcfdef0e6c5133c7db90602515d93bb72a68976d
jaquisonDev <jaquison1000@gmail.com>
2026-07-28 01:20:41 -0300

    Corrigindo vulnerabilidades e atualizando as bibliotecas

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,68 +1,67 @@
-import * as C from '../../Component/Components';
-import logo from '../assets/Logo S.png';
-import { useNavigate } from 'react-router-dom';
-import { Validation } from '../../Validation/validate';
-import { useState } from 'react';
+import * as C from "../../Component/Components";
+import logo from "../../assets/Logo S.png";
+import { useNavigate } from "react-router-dom";
+import { Validation } from "../../Validation/validate";
+import { useState } from "react";
 
 export const Login = () => {
+  const [fieldEmail, setFieldEmail] = useState("");
+  const [fieldPass, setFieldPass] = useState("");
 
-    const [fieldEmail, setFieldEmail] = useState('');
-    const [fieldPass, setFieldPass] = useState('');
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
-    
-    const validFields = () => {
-        let validation = Validation.fieldsLogin(fieldEmail, fieldPass);
-        
-        if(validation.fieldEmail && validation.fieldPass) {
-            navigate('/home');
-        }
+  const validFields = () => {
+    let validation = Validation.fieldsLogin(fieldEmail, fieldPass);
 
-        if(fieldEmail === "" && fieldPass === "") {
-            alert("Preencha todos os campos!!!");
-            return;
-        }
-
-        if(fieldEmail === "") {
-            alert("Preencha o email!! Não pode ser vazio.");
-            return;
-        }
-
-        if(fieldPass === "") {
-            alert("Preencha à Senha!! Não pode ser vazio.");
-            return;
-        }
-
+    if (validation.fieldEmail && validation.fieldPass) {
+      navigate("/home");
     }
 
-    return(
-        <C.ContainerLogin>
-            <C.AreaLogo>
-                <img src={logo} alt="Imagem da logo do serlivre" width={600}/>
-            </C.AreaLogo>
+    if (fieldEmail === "" && fieldPass === "") {
+      alert("Preencha todos os campos!!!");
+      return;
+    }
 
-            <C.AreaLogin>
-                <C.Title>Login</C.Title>
+    if (fieldEmail === "") {
+      alert("Preencha o email!! Não pode ser vazio.");
+      return;
+    }
 
-                <C.Formulary onSubmit={Validation.submitForm}>
-                    <C.Inputs 
-                        type="text" 
-                        placeholder='email...'
-                        value={fieldEmail}
-                        onChange={e => setFieldEmail(e.target.value)}
-                    />
-                    <C.Inputs 
-                        type="password" 
-                        placeholder='Senha...'
-                        value={fieldPass}
-                        onChange={e => setFieldPass(e.target.value)}
-                    />
+    if (fieldPass === "") {
+      alert("Preencha à Senha!! Não pode ser vazio.");
+      return;
+    }
+  };
 
-                    <C.ButtonLogin onClick={validFields}>Logar Adm</C.ButtonLogin>
-                    <C.LinkPassword href='#'>Esqueci minha senha? Clique aqui!!</C.LinkPassword>
-                </C.Formulary>
+  return (
+    <C.ContainerLogin>
+      <C.AreaLogo>
+        <img src={logo} alt="Imagem da logo do serlivre" width={600} />
+      </C.AreaLogo>
 
-            </C.AreaLogin>
-        </C.ContainerLogin>
-    ); 
-}
+      <C.AreaLogin>
+        <C.Title>Login</C.Title>
+
+        <C.Formulary onSubmit={Validation.submitForm}>
+          <C.Inputs
+            type="text"
+            placeholder="email..."
+            value={fieldEmail}
+            onChange={(e) => setFieldEmail(e.target.value)}
+          />
+          <C.Inputs
+            type="password"
+            placeholder="Senha..."
+            value={fieldPass}
+            onChange={(e) => setFieldPass(e.target.value)}
+          />
+
+          <C.ButtonLogin onClick={validFields}>Logar Adm</C.ButtonLogin>
+          <C.LinkPassword href="#">
+            Esqueci minha senha? Clique aqui!!
+          </C.LinkPassword>
+        </C.Formulary>
+      </C.AreaLogin>
+    </C.ContainerLogin>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  // Nome exato do repositório no GitHub entre barras
+  base: "/projetosr/",
+  build: {
+    // Garante que os arquivos gerados vão para a pasta 'dist'
+    outDir: "dist",
+  },
+});
